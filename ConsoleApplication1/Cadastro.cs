30025a81fa55fea0a55ec7f286f8f058c54a7a0e
Rodolfo Fadino <rodolfo.fadino@gmail.com>
2015-02-01 23:42:33 -0200

    First commit

---
+++ ConsoleApplication1/Cadastro.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Cadastro
+    {
+        public int Id { get; set; }
+        public string EmailPrincipal { get; set; }
+        public string NomeCompleto { get; set; }
+        public DateTime? DataNascimento { get; set; }
+    }
+
+    public class CadastroContext : DbContext
+    {
+        public DbSet<Cadastro> Cadastros { get; set; }
+        public CadastroContext()
+            : base("Cadastro")
+        {
+            Database.SetInitializer<CadastroContext>(null);
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new CadastroConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+    public class CadastroConfiguration : EntityTypeConfiguration<Cadastro>
+    {
+        public CadastroConfiguration()
+        {
+            ToTable(tableName: "CadastroExemplo", schemaName: "dbo");
+            Property(x => x.NomeCompleto).HasColumnName("Nome");
+            Property(x => x.EmailPrincipal).HasColumnName("Email").HasColumnType("varchar");
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+static void Main(string[] args)
+{
+    using (var context = new CadastroContext())
+    {
+        //Log da consulta que será realizada
+        context.Database.Log = Console.Write;
+
+        var cadastros = context.Cadastros.Where(a => a.EmailPrincipal == "rodolfo.fadino@gmail.com").ToList();
+
+        foreach (var cadastro in cadastros)
+        {
+            Console.WriteLine("----- cadastro ------");
+            Console.WriteLine(cadastro.Id);
+            Console.WriteLine(cadastro.NomeCompleto);
+        }
+    }
+}
+    }
+}
